7283cbed269dc5b95a2d31c4677945bab82c6fcf
Eldar <eldar.kasmamytov@gmail.com>
2020-09-23 16:02:03 +0200

    Added "Sentinelle" and "Pointeur Apres Suivant"
    - pointeur apres-suivant
    - sentinelle
    - debut de liste triee

---
+++ tp2/Liste/c++/src/main.cpp
@@ -0,0 +1,45 @@
+// LIFAP6 - Automne 2017 - R. Chaine
+
+#include "element.h"
+#include "liste.h"
+#include <cstdio>
+
+int main()
+{
+  // Liste lili;
+  // std::printf("Lili\n");
+  // lili.affichage();
+  // for(int i=1;i<5;i++)
+  //   lili.ajoutEnTete(i);
+  // std::printf("Lili\n");
+  // lili.affichage();
+  // for(int i=10;i<15;i++)
+  //   lili.ajoutEnQueue(i);
+  // std::printf("Lili\n");
+  // lili.affichage();
+  // Liste lolo(lili);
+  // std::printf("Lolo\n");
+  // lolo.affichage();
+  // lili.vide();
+  // std::printf("Lolo\n");
+  // lolo.affichage();
+  // lolo=lili;
+  // printf("Lolo\n");
+  // lolo.affichage();
+
+  Liste l;
+
+  std::cout << "Ajout en tete de 100 : " << std::endl;
+  l.ajoutEnTete(100);
+  l.affichage();
+
+  std::cout << "Ajout en tete de 99 : " << std::endl;
+  l.ajoutEnTete(99);
+  l.affichage();
+
+  std::cout << "Ajout en tete de 98 : " << std::endl;
+  l.ajoutEnTete(98);
+  l.affichage();
+
+  return 0;
+}
---
+++ tp2/ListeTriee/c++/src/liste_triee.cpp
@@ -0,0 +1,54 @@
+#include "liste_triee.h"
+
+Liste::Liste() {
+	ad = new Cellule;
+	ad->suivant = nullptr;
+	ad->apres_suiv = nullptr;
+	count = 0;
+}
+
+Liste::Liste(const Liste & l) {
+}
+
+Liste::~Liste() {
+	Cellule* current_address = ad;
+
+	Cellule* next_address;
+	if (current_address != nullptr)
+		next_address = current_address->suivant;
+
+	for (unsigned int i=0; i<=count+1; ++i) {
+		delete current_address;
+
+		if (next_address != nullptr) {
+			current_address = next_address;
+			next_address = next_address->suivant;
+		}
+	}
+}
+
+void Liste::ajoutEnTete(const Elem & e) {
+	Cellule* temp = ad->suivant;
+	ad->suivant = new Cellule;
+	ad->suivant->info = e;
+	ad->suivant->suivant = temp;
+	ad->suivant->apres_suiv = temp ? (temp->suivant ? temp->suivant : nullptr) : nullptr;
+	++count;
+}
+
+void Liste::affichage() const {
+	unsigned int i = 0;
+	Cellule* current_address = ad->suivant;
+
+	while(current_address != nullptr) {
+		std::cout << i << ": " << std::endl;
+		std::cout << "	info: " << current_address->info << std::endl;
+		std::cout << "	suivant: " << current_address->suivant << std::endl;
+		std::cout << "	apres suivant: " << current_address->apres_suiv << std::endl;
+
+		++i;
+		current_address = current_address->suivant;
+	}
+}
+
+bool Liste::testVide() const { return count == 0; }
